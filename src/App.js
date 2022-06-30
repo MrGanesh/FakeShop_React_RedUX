7307550b5f14a7bd4c5c9ac0935bdb8c29f573c7
MrGanesh <ganeshkulkarni689@gmail.com>
2022-06-30 17:07:24 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./style.css";
 import {Header} from "./containers/Header"
-import {BrowserRouter as Route, Switch, Router} from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from 'react-router-dom';
 import {ProductListing} from './containers/ProductListing'
 import {ProductDetail} from './containers/ProductDetail'
 export default function App() {
@@ -10,8 +13,8 @@ export default function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/" exact component={ProductListing} />
-          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route path="/" exact  component={ProductListing} />
+          <Route path="/product/:productId" exact  component={ProductDetail} />
           <Route> 404 Not Found </Route>
         </Switch>
       </Router>
